a06e446395fc47fd8cad566516356c115cc491b0
alejandro995 <alejodt995@gmail.com>
2016-11-20 00:03:38 -0500

    Se corriejieron errorres de compilacion y de carga de los archivos

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
 import com.avaje.ebean.Model;
+import com.avaje.ebean.Transaction;
 import controllers.*;
 import controllers.routes;
 import models.User;
@@ -13,6 +15,7 @@ import play.data.Form;
 import views.html.*;
 
 import javax.inject.Inject;
+import javax.persistence.PersistenceException;
 import java.util.List;
 
 import static play.data.Form.form;
@@ -40,6 +43,44 @@ public class UserController extends Controller{
         return ok(toJson(users));
 
     }
+
+    public Result edit(Long id) {
+        Form<User> formulario = formFactory.form(User.class).fill(User.find.byId(id));
+        return ok(
+                views.html.gestionarUser.render(formulario)
+        );
+    }
+
+    public Result updateUser(Long id) throws PersistenceException {
+        Form<User> formulario = formFactory.form(User.class).bindFromRequest();
+        if(formulario.hasErrors()){
+            return badRequest(views.html.user.render("erroneo"));
+        }
+
+        Transaction txn = Ebean.beginTransaction();
+
+        try{
+            User savedUser = User.find.byId(id);
+            if (savedUser != null) {
+                User newUser = formulario.get();
+                savedUser.email = newUser.email;
+                savedUser.pass = newUser.pass;
+                savedUser.username = newUser.username;
+
+                savedUser.update();
+                flash("success", "Usuario" + formulario.get().username + "has been update" );
+                txn.commit();
+
+            }
+
+
+        }finally {
+            txn.end();
+        }
+        return redirect("/");
+    }
+
+
 
 
     }
